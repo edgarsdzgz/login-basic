2c6f1cfd93d348469e3191a48dba6c7a40bc7a7d
Edgar Diaz-Gutierrez <edgar.s.dzgz@gmail.com>
2024-04-03 13:53:37 -0400

    First commit; working server listening on port 3000

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+// Start listening for requests on this express app server
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`The server is listening on port ${port}`));
+//# sourceMappingURL=index.js.map
